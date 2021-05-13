03bb8cfc9369e93d34a77285411d71b92cede5dd
isaac-shaw <83607986+isaac-shaw@users.noreply.github.com>
2021-05-12 18:38:48 -0700

    update1: Problem with printing the maze

--- project1.java
+++ project1.java
@@ -34,33 +34,58 @@ public class project1 extends Application{
    //Create needed Main Member Variables
    FlowPane root;
    Scene scene;
-   Graphics image;
-   GraphicsContext gc = new GraphicsContext2D();
+   Canvas image = new Canvas();
+   GraphicsContext gc = image.getGraphicsContext2D();
    
    int[][] maze = new int[25][25];
    
    public void start(Stage stage){
-      //Read in Maze file and put it in a 2D array
-      Scanner read = new Scanner(new File("mazeFile"));
-      for(int i = 0; i < 25; i++){
-         for(int j = 0; j < 25; j++){
-            maze[i][j] = read.netInt();
+      //Read in Maze file and put it in a 2D array with a try/catch
+      try{
+         Scanner read = new Scanner(new File("mazeFile"));
+         for(int i = 0; i < 21; i++){
+            for(int j = 0; j < 21; j++){
+               maze[i][j] = read.nextInt();
+               System.out.print(maze[i][j] + " ");
+            }
+            System.out.println();
          }
+      }catch(FileNotFoundException fnfe){
+         System.out.println("File not Found :(");
+      }
+      
+      //Paint the maze
+      image.setWidth(525);
+      image.setHeight(525);
+      int x = 0, y = 0;
+      
+      for(int i = 0; i < 21; i++){
+         for(int j = 0; j < 21; j++){
+            if(maze[i][j] == 0){
+               gc.setFill(Color.WHITE);
+               gc.fillRect(x, y, 25, 25);
+               x = x + 25;
+            }else if(maze[i][j] == 1){
+               gc.setFill(Color.BLACK);
+               gc.fillRect(x, y, 25, 25);
+               x = x + 25;
+            }
+         }
+         y = y + 25;
+         System.out.println(y + " ");
       }
       
       //Create root and set pref size
       root = new FlowPane();
       root.setPrefSize(800, 200);
       
-      //Create the Drawer class and add it to root
-      image = new Graphics(525, 525);
-      
+      //Add the canvas to root
       root.getChildren().add(image);
       root.setOnKeyPressed(new KeyPressedHandler());
       
       //Set Scence
-      scene = new Scene(root, 800, 200);
-      stage.setTitle("My First Animation");
+      scene = new Scene(root, 525, 525);
+      stage.setTitle("A Fun Little Maze");
       stage.setScene(scene);
       stage.show();
       
@@ -71,40 +96,19 @@ public class project1 extends Application{
    //Key Handler Class for awsd
    public class KeyPressedHandler implements EventHandler<KeyEvent>{
       public void handle(KeyEvent e){
-         switch(e.getCode()){
-            case KeyCode.A:
+         switch(e.getText()){
+            case "a":
                break;
-            case KeyCode.W:
+            case "w":
                break;
-            case KeyCode.S:
+            case "s":
                break;
-            case KeyCode.D:
+            case "d":
                break;
          }
       }
    }
    
-   //Drawer class for loading bar
-   public class Graphics extends Canvas{
-      
-      //Create (and instantiate) variables
-      private int x, y;  
-
-      public Graphics(int width, int height){
-         
-         this.width = width;
-         this.height = height;
-         
-         setWidth(width);
-         setHeight(height);
-         
-      }
-
-      public void draw(GraphicsContext gc){
-      }
-   } 
-  
-  
    //Launch the program
    public static void main(String[] args)
    {
